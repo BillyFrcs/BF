a99cad52b7f15c5a09a65ade9e06d2aef87df61c
Billy Franscois <billyfranscois@outlook.com>
2021-10-04 21:25:25 +0900

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,26 +8,20 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I'm <span className="purple">Billy Franscois </span>
-            <br />I'm a Computer Science Student and beginner Game Developer.
+            Hi Everyone, I'm <span className="purple">Billy Franscois, </span>
+            I'm a Computer Science Student and beginner Game Developer.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from making my games, some other activities that I like to do !
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> 3D Modelling
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Listen music
+              <ImPointRight /> Playing Video Games 🎮
             </li>
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Doing what you like to make you happy🙂"{" "}
           </p>
           <footer className="blockquote-footer">Billy</footer>
         </blockquote>
