5f37941a47f3cfd9caacab6910d6b2a09dea4a0c
Billy Franscois <billyfranscois@outlook.com>
2021-10-04 20:05:11 +0900

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -65,7 +65,7 @@ function NavBar() {
                 to="/about"
                 onClick={() => updateExpanded(false)}
               >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> Billy
               </Nav.Link>
             </Nav.Item>
 
