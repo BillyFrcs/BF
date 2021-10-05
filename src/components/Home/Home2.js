b2876af887639a16883dceef846abf2b2ceb405a
Billy Franscois <billyfranscois@outlook.com>
2021-10-05 10:24:32 +0900

    New changes Home section

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,16 +16,17 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              Let Me <span className="purple"> Introduce </span> MySelf
+              Getting <span className="purple"> Started </span>
             </h1>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
               something, I think… 🤷‍♂️
               <br />
-              <br />I am fluent in classics like
+              <br />At the moment, i'm using 
               <i>
-                <b className="purple"> C++, And C#. </b>
+                <b className="purple"> C++ and C# </b>
               </i>
+              as my main language because i'm using <b className="purple"> Unity Game Engine</b> to developed my games.
               <br />
               <br />
               My field of Interest's are building new &nbsp;
@@ -33,13 +34,13 @@ function Home2() {
                 <b className="purple">Game Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Game design and something related.
+                  Game Design.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Unity and Unreal Engine.</b>
+              Whenever possible, I also apply my passion for learning and developing products
+              with <b className="purple">Unreal Engine </b>even though i'm still learning the engine😁.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
