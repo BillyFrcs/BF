eca3b87e76f12dbd492e35164408ae71adde2573
Billy Franscois <billyfranscois@outlook.com>
2021-12-20 10:49:38 +0900

    New changes AboutCard

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -14,9 +14,9 @@ function AboutCard()
             <br />
             I like to playing games🎮 So in this way I can improve my skills
             and move on to the new things. And i decided to learn something
-            new about <span className="purple">Game Development</span>.
+            new about <span className="purple">Game Development.</span> My stronger and passionate is to create cool <span className="purple">Game Mechanics</span> and Scripting with Programming Language and then doing some Debugging and Testing to find bugs and <span className="purple">Optimize Games.</span>
             <br />
-            I've already released a few Games that i've been working by myself. you can go to Games section to see my released Games. And if you want to see my Projects you can go to Projects section.
+            I've already released a few Games that i've been working by myself. You can go to my Games section to see my released Games. And if you want to see my Projects you can go to my Projects section.
             <br />
             <br />
             Apart from learning and making games, some other activities that I like to do !
