365af9d9eb464f5e62ab1503d08584c730977270
Billy Franscois <billyfranscois@outlook.com>
2022-01-28 21:45:14 +0900

    Fixed shit!

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+
+// import Button from "react-bootstrap/Button";
+
 import Particle from "../Particle";
+
 // import pdf from "../../Assets/Resume/.pdf"; // Put pdf path here
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+// import { AiOutlineDownload } from "react-icons/ai";
+
+import { /*Document, Page,*/ pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = ""; // Put the pdf link here
+// const resumeLink = ""; // Put the pdf link here
 
 function Resume()
 {
-     const [width, setWidth] = useState(1200);
+     const [/*width,*/ setWidth] = useState(1200);
 
      useEffect(() =>
      {
