d092f3293b2defaf928233674a71ee481093a26e
Billy Franscois <BillyFranscois19@outlook.com>
2022-03-17 13:40:27 +0900

    Update Project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,39 +21,30 @@ function Projects()
           Here are my <span className="purple">Open Source Projects</span> that I've been working on.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card snake-boxes">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={racereternity}
               isBlog={false}
               title="Racer Eternity"
-              description="Racer Eternity is a simple 3D car driving simulator with advanced physics engine & simulation."
+              description="Racer Eternity is a 3D car driving simulator with high level Car Controller and implement advanced physics engine & simulation."
               link="https://github.com/BillyFrcs/RacerEternity"
             />
           </Col>
-          <Col md={4} className="project-card snake-boxes">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={coronarunner}
               isBlog={false}
               title="Corona Runner"
-              description="Corona Runner is a 2D endless runner Game that theme about Corona Virus created using Unity Engine."
+              description="Corona Runner is a 2D endless runner Game that Player should be able to avoid the Corona Virus when they spawned non-stop."
               link="https://github.com/BillyFrcs/CoronaRunner"
             />
           </Col>
-          <Col md={4} className="project-card snake-boxes">
-            <ProjectCard
-              imgPath={fireworks}
-              isBlog={false}
-              title="Fireworks VFX"
-              description="Fireworks VFX created using Visual Effect Graph from Unity & High Definition Render Pipeline(HDRP)."
-              link="https://github.com/BillyFrcs/FireworksVFX"
-            />
-          </Col>
-          <Col md={4} className="project-card snake-boxes">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={shooterglow}
               isBlog={false}
               title="Shooter Glow"
-              description="Simple 2D top down Shooter Game, Shooter Glow with beautiful color and glowing effect (Post Processing)."
+              description="Simple 2D top down Shooter Game, Shooter Glow with beautiful color and glowing effect to improve Gameplay experience."
               link="https://github.com/BillyFrcs/ShooterGlow"
             />
           </Col>
