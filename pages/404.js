814eb6ef2983a41c099b4e6d8011efb96c2e81ed
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-06 22:17:23 +0900

    New Changes

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,13 @@
 import NextLink from 'next/link'
 import
-  {
-    Box,
-    Heading,
-    Text,
-    Container,
-    Divider,
-    Button
-  } from '@chakra-ui/react'
+{
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button
+} from '@chakra-ui/react'
 
 const NotFound = () =>
 {
