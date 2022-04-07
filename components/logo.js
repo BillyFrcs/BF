1774c73d382c10482554d0219bc8b52715a7102e
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-07 22:47:31 +0900

    Update Logo

--- components/logo.js
+++ components/logo.js
@@ -36,7 +36,7 @@ const Logo = () =>
             fontWeight="bold"
             ml={3}
           >
-            Billy Franscois
+            Billy
           </Text>
         </LogoBox>
       </a>
