df2c694bf3c5fd0d4cfa35f51a9b845c030aaeb5
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-08 10:48:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: ""
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig;
