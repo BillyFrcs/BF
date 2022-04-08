917f1acb458a7673e7a19659390a6fdebb91e970
Billy Franscois <BillyFranscois19@outlook.com>
2022-04-09 00:53:05 +0900

    Comment Unused import

--- pages/works/BillGuys.js
+++ pages/works/BillGuys.js
@@ -1,6 +1,6 @@
-import { Container, Badge, Link, List, ListItem, Heading, Center } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem /*, Heading, Center*/ } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, WorkImage, Meta } from '../../components/work'
+import { Title, /*WorkImage,*/ Meta } from '../../components/work'
 import Paragraph from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
